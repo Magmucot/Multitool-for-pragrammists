49a182926a28965c087e0987f9fdded3b70c0a50
Magmucot <magmucot@gmail.com>
2025-04-05 14:13:23 +0700

    Update Grafics.py

--- Grafics.py
+++ Grafics.py
@@ -1,4 +1,4 @@
-from curses.ascii import isalnum
+from curses.ascii import isdigit
 import sys
 import os
 from PySide6.QtWidgets import (QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout,
@@ -23,27 +23,33 @@ class MainWindow(QMainWindow):
         self.main_layout.setContentsMargins(10, 10, 10, 10)
 
         self.btn_style = """
-            QPushButton { background-color: #FCA300; border-radius: 4px; border: none; }
+            QPushButton { background-color: #FCA300; border-radius: 2px; border: none; }
             QPushButton:hover { background-color: #A04602; }
         """
         self.btn_active_style = """
-            QPushButton { background-color: #A04602; border-radius: 4px; border: none; }
+            QPushButton { background-color: #A04602; }
+        """
+        self.btn_inactive_style = """
+            QPushButton { background-color: #FCA300;}
         """
         
         self.setStyleSheet(self.btn_style)
 
         self.up_layout = QHBoxLayout()
         self.btn_shifr = QPushButton()
+        self.btn_shifr.setStyleSheet(
+            '''QPushButton:{border-radius: 2px; border: none}''')
         self.btn_shifr.setIcon(QIcon.fromTheme("document-encrypt"))
         self.btn_shifr.setFixedSize(40, 40)
-        
+        self.up_layout.addWidget(self.btn_shifr)
 
         self.btn_transf = QPushButton('1011')
         self.btn_transf.setFixedSize(40, 40)
         
-        self.up_layout.addWidget(self.btn_shifr)
+        
         self.up_layout.addWidget(self.btn_transf)
-        self.main_layout.addStretch()
+        self.up_layout.addStretch()
+        
         self.main_layout.addLayout(self.up_layout)
         
         separator = QFrame()
@@ -57,14 +63,20 @@ class MainWindow(QMainWindow):
         self.shifr_page()
         self.transf_page()
 
-        self.btn_shifr.clicked.connect(self.shifr_p_btn_click)
-        self.btn_transf.clicked.connect(self.transf_p_btn_click)
+        self.btn_shifr.clicked.connect(self.show_shifr_p)
+        self.btn_transf.clicked.connect(self.show_transf_p)
 
-    def shifr_p_btn_click(self):
-        pass
+    def show_shifr_p(self):
+        self.stack.setCurrentIndex(0)
+        self.setStyleSheet(self.btn_style)
+        self.btn_shifr.setStyleSheet(self.btn_active_style)
+        self.btn_transf.setStyleSheet(self.btn_inactive_style)
 
-    def transf_p_btn_click(self):
-        pass
+    def show_transf_p(self):
+        self.stack.setCurrentIndex(1)
+        self.setStyleSheet(self.btn_style)
+        self.btn_transf.setStyleSheet(self.btn_active_style)
+        self.btn_shifr.setStyleSheet(self.btn_inactive_style)
 
     def shifr_page(self):
 
@@ -117,7 +129,7 @@ class MainWindow(QMainWindow):
             QMessageBox.warning(self, 'Ошибка!',
                                 'Ошибка!\nЗаполните все поля!')
             return
-        if tip == 'Цезарь' and not isinstance(key, int):
+        if tip == 'Цезарь' and not isdigit(key):
             QMessageBox.warning(self, 'Ошибка!',
                                 'Ошибка!\nКлюч для цезаря всегда целое число!')
             return
@@ -152,12 +164,16 @@ class MainWindow(QMainWindow):
 
         self.input_numb1 = QLineEdit()
         self.input_numb1.setPlaceholderText('Введите число(а)')
+        
         self.input_osn1 = QLineEdit()
         self.input_osn1.setPlaceholderText('Введите Основание(я) 1')
+        
         self.input_osn2 = QLineEdit()
         self.input_osn2.setPlaceholderText('Введите Основание(я) 2')
+        
         btn = QPushButton('Ввод')
         self.res = QLineEdit('Результат')
+        
         self.res.setReadOnly(True)
         btn.clicked.connect(self.btn_transf_click)
 
@@ -170,8 +186,87 @@ class MainWindow(QMainWindow):
         layout.addLayout(layout1)
         layout.addSpacing(20)
 
-        self.stack.addWidget(page)
+        math_layout = QHBoxLayout()
+        self.m_num1 = QLineEdit()
+        self.m_num1.setPlaceholderText('Введите число')
+        math_layout.addWidget(self.m_num1)
 
+        self.m_base1 = QLineEdit()
+        self.m_base1.setPlaceholderText('Введите основание')
+        math_layout.addWidget(self.m_base1)
+        
+        self.operation = QComboBox()
+        self.operation.addItems(["+", "-", "×", "÷"])
+        self.operation.setFixedWidth(50)
+        math_layout.addWidget(self.operation)
+        
+        self.m_num2 = QLineEdit()
+        self.m_num2.setPlaceholderText('Введите число')
+        math_layout.addWidget(self.m_num2)
+
+        self.m_base2 = QLineEdit()
+        self.m_base2.setPlaceholderText('Введите основание')
+        math_layout.addWidget(self.m_base2)
+        
+        self.math_btn = QPushButton('Выполнить')
+        self.math_btn.clicked.connect(self.math_btn_click)
+        math_layout.addWidget(self.math_btn)
+        
+        self.res_base = QLineEdit()
+        self.res_base.setPlaceholderText('Основание ответа')
+        self.res_base.setFixedWidth(50)
+        math_layout.addWidget(self.res_base)
+        
+        self.res_math = QLineEdit("Результат")
+        self.res_math.setReadOnly(True)
+        self.res_math.setFixedWidth(100)
+        math_layout.addWidget(self.res_math)
+        
+        
+        layout.addLayout(math_layout)
+        layout.addStretch()
+        self.stack.addWidget(page)
+        
+    def math_btn_click(self):
+        numb1, numb2 = str(self.m_num1.text().strip()), str(self.m_num2.text().strip())
+        res_base, base1, base2 = self.res_base.text(), str(self.m_base1.text()), str(self.m_base2.text())
+        
+        operation = self.operation.currentText()
+
+        if not numb1 or not numb2:
+            QMessageBox.warning(
+                self, "Ошибка!", "Введите оба числа!")
+            return
+        try:
+            if not self.res_base:
+                res_base = 10
+            if not base1 or not base2:
+                digits = set(numb1 + numb2)
+                max_digit = max([int(d, 16) for d in digits if d.isalnum()])
+                auto_base = max_digit + 1
+                base1 = auto_base if not base1 else int(base1)
+                base2 = auto_base if not base2 else int(base2)
+            else:
+                res_base = int(res_base)
+                base1 = int(base1)
+                base2 = int(base2)
+            if base1 < 2 or base2 < 2 or res_base < 2:
+                QMessageBox.warning(self, "Ошибка!", "Система счисления должна быть >= 2!")
+                return
+            num1 = int(numb1, base1)
+            num2 = int(numb2, base2)
+        except ValueError as e:
+            QMessageBox.warning(self, "Ошибка!", f"Неправильный ввод: {str(e)}")
+            return
+
+        if operation == "÷" and num2 == 0:
+            QMessageBox.warning(
+                self, "Ошибка!", "Деление на ноль!")
+            return
+        oper = {"×": '*', '+': '+', '-': "-", '÷': '/'}
+        math = Transformator()
+        result = str(math.math_oper(num1, base1, oper[operation], num2, base2))
+        self.res_math.setText(result)
 
 if __name__ == "__main__":
     os.environ["QT_QPA_PLATFORM"] = "xcb"
